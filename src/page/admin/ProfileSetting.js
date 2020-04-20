0d125182bebae7c0e24fc018f97766213018e85b
sonthh <tranhuuhongson@gmail.com>
2020-04-20 20:51:23 +0700

    init project

--- src/page/admin/ProfileSetting.js
+++ src/page/admin/ProfileSetting.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import { Breadcrumb, Tabs } from 'antd';
-import MediaQuery from 'react-responsive'
+import responsive from '../../constant/responsive'
 
 const { TabPane } = Tabs;
 
 export default class ProfileSetting extends Component {
 
-  renderTabs = (position) => {
-    return (
-      <Tabs defaultActiveKey="1" tabPosition={position}>
-        <TabPane tab="Basic Settings" key="1">
-          Content of Tab Pane 1
-        </TabPane>
-        <TabPane tab="Security Settings" key="2">
-          Content of Tab Pane 2
-        </TabPane>
-        <TabPane tab="Account Binding" key="3">
-          Content of Tab Pane 3
-        </TabPane>
-        <TabPane tab="Message Notification" key="4">
-          Content of Tab Pane 4
-        </TabPane>
-      </Tabs>
-    );
+  state = {
+    position: 'left',
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize);
+    this.resize();
+  }
+
+  resize = () => {
+    const { innerWidth } = window;
+    const isLargeScreen = innerWidth >= responsive.md;
+    const position = isLargeScreen ? 'left' : 'top';
+
+    if (position !== this.state.position) {
+      this.setState({ position });
+    }
   }
 
   render() {
@@ -32,12 +32,20 @@ export default class ProfileSetting extends Component {
           <Breadcrumb.Item>User</Breadcrumb.Item>
           <Breadcrumb.Item>Profile</Breadcrumb.Item>
         </Breadcrumb>
-        <MediaQuery maxWidth={767}>
-          {this.renderTabs('top')}
-        </MediaQuery>
-        <MediaQuery minWidth={767}>
-          {this.renderTabs('left')}
-        </MediaQuery>
+        <Tabs defaultActiveKey="1" tabPosition={this.state.position}>
+          <TabPane tab="Basic Settings" key="1">
+            Content of Tab Pane 1
+        </TabPane>
+          <TabPane tab="Security Settings" key="2">
+            Content of Tab Pane 2
+        </TabPane>
+          <TabPane tab="Account Binding" key="3">
+            Content of Tab Pane 3
+        </TabPane>
+          <TabPane tab="Message Notification" key="4">
+            Content of Tab Pane 4
+        </TabPane>
+        </Tabs>
       </>
     );
   }
