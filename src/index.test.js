186633c4884ebc7126a9f03506ea60bc1b61f1b0
sonthh <tranhuuhongson@gmail.com>
2020-05-03 15:37:56 +0700

    testing integration

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+describe('Basic testing', () => {
+  it('Sum', () => {
+    expect(1 + 1).toBe(2);
+  })
+})
+
+describe('Hello world', () => {
+  it('helloworld', () => {
+    expect('helloworld').toBe('helloworld');
+  })
+})
